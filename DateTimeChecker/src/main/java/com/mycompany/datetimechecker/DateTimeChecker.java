23c14db7a4de4b84c3bec9b8a0b8df04f04fffdf
Hữu Minh <minhnphse160370@fpt.edu.vn>
2022-09-27 11:23:20 +0700

    Update image import and add Playwright dependences

--- DateTimeChecker/src/main/java/com/mycompany/datetimechecker/DateTimeChecker.java
+++ DateTimeChecker/src/main/java/com/mycompany/datetimechecker/DateTimeChecker.java
@@ -9,9 +9,17 @@ package com.mycompany.datetimechecker;
  *
  * @author Huu Minh
  */
+
+import com.microsoft.playwright.*;
+
 public class DateTimeChecker {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    try (Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.chromium().launch();
+      Page page = browser.newPage();
+      page.navigate("http://playwright.dev");
+      System.out.println(page.title());
     }
+  }
 }
---
+++ DateTimeChecker/src/main/java/com/mycompany/datetimechecker/GetDirectory.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.datetimechecker;
+
+import java.io.File;
+
+
+public class GetDirectory {
+
+    private static final File file = new File("");
+    public static String DIR = file.getAbsolutePath();
+     
+    
+    
+   
+}
